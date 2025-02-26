356b3a1741a3db57bf6c3976998b289ee0c7feb8
SleepingCui <mxingfoew1034@outlook.com>
2025-02-26 22:06:14 +0800

    再次更改log4j2配置

--- Judger/src/main/java/org/bcmoj/judgeserver/JudgeServer.java
+++ Judger/src/main/java/org/bcmoj/judgeserver/JudgeServer.java
@@ -1,6 +1,10 @@
 package org.bcmoj.judgeserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.bcmoj.judger.Judger;
+
+import java.io.File;
 
 public class JudgeServer {
+
 }
