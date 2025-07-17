c87bb93aaae807fae6651a502d2e929ae9e1ee98
SleepingCui <130898297+SleepingCui@users.noreply.github.com>
2025-07-17 18:23:24 +0800

    1.0.6-alpha

--- webapp/app/core/__init__.py
+++ webapp/app/core/__init__.py
@@ -2,7 +2,7 @@ from .db import db, DB_URI, init_db
 from .db import User, Problem, JudgeResult, CheckpointResult, Example
 from .logger import setup_logging, log_route_context
 
-version = "1.0.5-alpha"
+version = "1.0.6-alpha"
 
 __all__ = [
     'db',
