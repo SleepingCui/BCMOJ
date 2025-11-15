6c30aaea92f765a766de12d169740b9158844ab9
MxingFoew1034 <mxingfoew1034@163.com>
2025-11-15 23:57:43 +0800

    fix: 修复 favicon 路由路径错误 chore: 后端 API 移除 problems 字段

--- webapp/app/services/__init__.py
+++ webapp/app/services/__init__.py
@@ -6,7 +6,7 @@ from .register import register_user
 from .problems import get_problems_list
 from .problem import get_problem_with_examples
 from .results import get_result_detail, get_results_list, check_user_authorization
-from .admin_api import get_admin_data, save_config_yml, update_user, delete_user, create_problem, update_problem, delete_problem
+from .admin_api import get_admin_data, save_config_yml, update_user, delete_user
 from .teacher_api import get_teacher_problem_data, teacher_create_problem, teacher_update_problem, teacher_delete_problem
 from .admin_results import get_admin_results
 from .about import fetch_contributors
@@ -32,9 +32,6 @@ __all__ = [
     'save_config_yml',
     'update_user',
     'delete_user',
-    'create_problem',
-    'update_problem',
-    'delete_problem',
     'get_teacher_problem_data',
     'teacher_create_problem',
     'teacher_update_problem',
