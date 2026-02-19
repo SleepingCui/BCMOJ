e49eb184b9dba316c576401f3d81345e7969161f
SleepingCui <130898297+SleepingCui@users.noreply.github.com>
2026-02-19 14:33:05 +0800

    1.0.14

--- webapp/app/core/__init__.py
+++ webapp/app/core/__init__.py
@@ -2,7 +2,7 @@ from .db import db, DB_URI, init_db
 from .db import User, Problem, JudgeResult, CheckpointResult, Example, ProblemGroup
 from .logger import setup_logging, log_route_context
 
-version = "1.0.14-beta"
+version = "1.0.14"
 
 __all__ = [
     'db',
